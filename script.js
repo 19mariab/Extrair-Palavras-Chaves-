7de4dd3615042a8aa7e1c03e46b5c2359ecd3c67
19mariab <biondo.araujo@escola.pr.gov.br>
2025-09-08 08:01:12 -0300

    aula 8.09

---
+++ script.js
@@ -0,0 +1,13 @@
+import {PALAVRAS_RUINS} from "/palavrasruins";
+
+const botaoMostraPalavra = document.querySelector ('#botao-palavrachave');
+
+botaoMostraPalavra.addEventListener ('click', mostraPalavrasChave);
+
+function mostraPalavrasChave () {
+    const texto = document.querySelector ('entrada-de-texto').value;
+    const campoResultado = document.querySelector ('#resultado-palavrachave');
+    const palavrasChave = processaTexto (texto);
+
+    campoResultado.textContent =palavrasChave.join (",")
+}
